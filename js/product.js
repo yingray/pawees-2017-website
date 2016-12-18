cd8e15e05fbf63c181cefd316ba6cd07a2c2b5b4
吳孟庭 <b98602046@ntu.edu.tw>
2016-12-19 02:00:21 +0800

    產品陳列頁_格言分頁完成

--- js/product.js
+++ js/product.js
@@ -32,7 +32,9 @@
 
 
 
-    var click_this_animalicon = $(".productclass_secondnav div div");
+// 動物分頁
+
+    var click_this_animalicon = $(".productclass_secondnav div.animalicon div");
     var this_animalicon_large = $(".product_item .this_animalicon_large div");
     var this_animalname = $(".this_animalname div");
 
@@ -53,6 +55,34 @@
 
         // 動物名字title
         this_animalname.text(animal_name);
+
+
+
+        // 按鈕點擊後往上捲到第二選單(才看得到下面有產品出來)
+        $('html,body').stop(true,false).animate({scrollTop:(productclass_secondnav.offset().top - header.outerHeight())},1000);
+
+    });
+
+
+
+// 格言分頁
+
+    var click_this_mottoicon = $(".productclass_secondnav div.mottoicon div");
+    var this_mottoname = $(".this_mottoname div");
+
+    click_this_mottoicon.click(function(){
+        // 先全部移除this_mottoicon
+        click_this_mottoicon.removeClass("this_mottoicon");
+
+        // 再抓classname
+        var mottoicon_name=$(this).attr("class");
+        var motto_name=mottoicon_name.slice(12);
+
+        // 再變色
+        $(this).addClass("this_mottoicon");
+
+        // 格言名字title
+        this_mottoname.text(motto_name);
 
 
 

1c093977224237941b85df4b43294614076e9c1c
吳孟庭 <b98602046@ntu.edu.tw>
2016-12-18 15:33:43 +0800

    產品陳列頁_單項產品背板完成

--- js/product.js
+++ js/product.js
@@ -4,13 +4,30 @@
 
 
     var product_logo = $(".product_logo div");
-    var product_item = $(".product_item");
+    var productclass_intro_content = $(".productclass_intro_content");
     var productclass_secondnav = $(".productclass_secondnav");
+    var product_item = $(".product_item");
+
 
 
     product_logo.click(function(){
+        // 全部先移除
         product_logo.removeClass("this_productclass");
+
+        // 取得現在點擊的類別字串
+        var product_class_name = $(this).attr("class");
+        var product_class_html = product_class_name + ".html" ; //字串串接出檔案名稱
+
+        // 加上新類別換內容
         $(this).addClass("this_productclass");
+
+
+        // ajax
+        // productclass_intro_content.load("ajax/product_animal.html");
+
+
+        console.log(product_class_html);
+
     })
 
 

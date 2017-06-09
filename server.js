bbc9139b551adfc463b348243de94a947a9be344
Yingray <yingray.lu@fuhu.com>
2017-06-09 23:01:15 +0800

    Updated the build and server config

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const path = require('path');
-const compression = require('compression');
-const app = express();
-const port = 8081;
+const express = require('express')
+const path = require('path')
+const compression = require('compression')
+const app = express()
+const port = 8081
 
-app.use(compression());
+app.use(compression())
 // app.use(express.static('./PAWEES'));
-app.use('/', express.static('./PAWEES'));
-app.use('/admin/', express.static('./build'));
+app.use('/', express.static(path.join(__dirname, 'PAWEES')))
+app.use('/admin/', express.static(path.join(__dirname, 'build')))
 
-app.get('/admin/*', function (req, res) {
-	res.sendFile(path.join(__dirname, './build', 'index.html'));
-});
+app.get('/admin/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, './PAWEES', 'index.html'));
-});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'PAWEES', 'index.html'))
+})
 
 app.listen(port, () => {
-	console.log(`Listening on port ${port} (http://localhost:${port})`);
-});
+    console.log(`Listening on port ${port} (http://localhost:${port})`)
+})
